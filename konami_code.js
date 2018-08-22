1612001c67c79678530bbd515b483f2b5c233b6f
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-22 16:23:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function onKeyDownHandler(event) {
   const key = parseInt(event.detail || event.which)
-  if (tracker ===code.length) {
+  if (tracker === code.length) {
       event.preventDefault()
       return
     }
